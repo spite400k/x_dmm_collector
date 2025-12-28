826c043b02c0682bf397f51b296e5a3a6c56fbd8
spite400k <spite400k@gmail.com>
2025-12-28 17:19:32 +0900

    修正

---
+++ run_x_dmm_collector_btlt.bat
@@ -0,0 +1,23 @@
+@echo off
+REM ================================================
+REM タスクスケジューラ用：x_dmm_collector 実行バッチ
+REM ================================================
+
+REM Pythonの絶対パスを指定（ここを自分の環境に置き換える）
+SET PYTHON_EXE=C:\Users\kazuk\AppData\Local\Programs\Python\Python313\python.exe
+REM スクリプトの絶対パス
+SET SCRIPT_PATH=c:/Users/kazuk/python/x_dmm_collector/main_bltl.py
+
+REM 作業ディレクトリ
+SET WORK_DIR=C:\Users\kazuk\python\x_dmm_collector
+
+REM ログ出力用ファイル
+SET LOG_FILE=%WORK_DIR%\\logs\task_run_bltl.log
+
+REM ================================================
+echo ================================================ >> %LOG_FILE%
+echo %DATE% %TIME% - タスク開始 >> %LOG_FILE%
+cd /d %WORK_DIR%
+"%PYTHON_EXE%" "%SCRIPT_PATH%" >> %LOG_FILE% 2>&1
+echo %DATE% %TIME% - タスク終了 >> %LOG_FILE%
+echo ================================================ >> %LOG_FILE%
